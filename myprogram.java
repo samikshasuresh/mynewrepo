cb9ef44afeb13a1b9c25d5ff5f9c26d3237f237d
samikshasuresh <105210966+samikshasuresh@users.noreply.github.com>
2022-05-11 15:36:22 +0530

    Add files via upload
    added

---
+++ myprogram.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class myprogram
+{
+public static void main(String[] args)
+{
+int a,b,c;
+Scanner obj = new Scanner(System.in);
+System.out.println("Enter A value:");
+a=obj.nextInt();
+System.out.println("Enter B value:");
+b=obj.nextInt();
+c=a+b;
+System.out.println("Result c:"+c);
+}
+}
